b6e36d4ed8bc3619598d171552d1306e2113dff2
Eike Decker <zet23t@googlemail.com>
2024-10-31 10:04:11 +0100

    Adding flame sprites

---
+++ src/resources/level_assets/anim_flame.png.meta.c
@@ -0,0 +1,22 @@
+{
+    "animations": [
+        {
+            "name": "red_flame",
+            "offsetX": 0,
+            "offsetY": 0,
+            "frameWidth": 32,
+            "frameHeight": 32,
+            "frameCount": 4,
+            "frameRate": 8
+        },
+        {
+            "name": "blue_flame",
+            "offsetX": 0,
+            "offsetY": 32,
+            "frameWidth": 32,
+            "frameHeight": 32,
+            "frameCount": 4,
+            "frameRate": 8
+        }
+    ]
+}
